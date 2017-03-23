262bb8fad2123c9eb00038af0ba33bd0cf95123e
Dan Patey <danpatey@danpatey.com>
2017-03-22 23:09:41 -0700

    Initial Commit

---
+++ ClosestCity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClosestCity
+//
+//  Created by Dan Patey on 3/22/17.
+//  Copyright © 2017 Dan Patey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ClosestCity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ClosestCity
+//
+//  Created by Dan Patey on 3/22/17.
+//  Copyright © 2017 Dan Patey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
