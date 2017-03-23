9636bf75de27450c1486310ad0c651a28c7a8b71
Dan Patey <danpatey@danpatey.com>
2017-03-23 00:40:53 -0700

    Saving this for later

--- ClosestCity/ViewController.m
+++ ClosestCity/ViewController.m
@@ -16,7 +16,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Consume our file and turn it into strings
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"US"
+                                                     ofType:@"txt"];
+    NSString* content = [NSString stringWithContentsOfFile:path
+                                                  encoding:NSUTF8StringEncoding
+                                                     error:NULL];
+    
+    // Create our two arrays
+    NSArray *arr = [content componentsSeparatedByString:@"\n"];
+    NSMutableArray *cityArr = [[NSMutableArray alloc] initWithCapacity: 5];
+
+    // Break the first huge array into a small second array
+    [cityArr insertObject:[NSMutableArray arrayWithObjects:@"0",@"0",@"0",@"0",@"0",nil] atIndex:0];
+    
+    // Print out our result
+    NSLog(@"%@", arr[0]);
 }
 
 
@@ -24,6 +40,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
